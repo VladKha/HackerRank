03cac99f22716f38e5abb1a3b2213975d4658195
vladkha <vladislavkhaitov@gmail.com>
2017-04-28 23:58:45 +0300

    Add 'Check Subset' to 'Sets'

--- Python/Sets/Check Subset/solve.py
+++ Python/Sets/Check Subset/solve.py
@@ -1,8 +1,7 @@
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        input()
-        a = set(input().split())
-        input()
-        b = set(input().split())
-        print(a.issubset(b))
+t = int(input())
+for _ in range(t):
+    input()
+    a = set(input().split())
+    input()
+    b = set(input().split())
+    print(a.issubset(b))
